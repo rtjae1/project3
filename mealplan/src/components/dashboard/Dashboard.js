2ea4b3d3e82ccfa5e2925b9556d8abd001122944
rtjae1 <ryan.t.jae@gmail.com>
2019-01-23 17:47:19 -0500

    syncing data from firestore

--- mealplan/src/components/dashboard/Dashboard.js
+++ mealplan/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Notifications from "./Notifications";
 import MealList from "../meals/MealList";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
   render() {
@@ -23,7 +25,11 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
-  return { meals: state.meal.meals };
+  console.log(state);
+  return { meals: state.firestore.ordered.meals };
 };
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "meals" }])
+)(Dashboard);
--- mealplan/src/store/reducers/rootReducer.js
+++ mealplan/src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from "./authReducer";
 import mealReducer from "./mealReducer";
 import { combineReducers } from "redux";
+import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  meal: mealReducer
+  meal: mealReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
