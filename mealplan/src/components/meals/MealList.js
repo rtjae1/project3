088ab3b52b9f3e918e405cd137403fa94fd729a7
rtjae1 <ryan.t.jae@gmail.com>
2019-01-23 11:50:11 -0500

    Meal Summry

--- mealplan/src/components/meals/MealList.js
+++ mealplan/src/components/meals/MealList.js
@@ -1,29 +1,13 @@
 import React from "react";
+import MealSummary from "./MealSummary";
 
 const MealList = () => {
   return (
     <div className="meal-list section">
-      <div className="card z-depth-0 meal-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Meal Title</span>
-          <p>Posted by wRyan</p>
-          <p className="grey-text">January 23</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 meal-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Meal Title</span>
-          <p>Posted by wRyan</p>
-          <p className="grey-text">January 23</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 meal-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Meal Title</span>
-          <p>Posted by wRyan</p>
-          <p className="grey-text">January 23</p>
-        </div>
-      </div>
+      <MealSummary />
+      <MealSummary />
+      <MealSummary />
+      <MealSummary />
     </div>
   );
 };
---
+++ mealplan/src/components/meals/MealSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MealSummary = () => {
+  return (
+    <div className="card z-depth-0 meal-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Meal Title</span>
+        <p>Posted by wRyan</p>
+        <p className="grey-text">January 23</p>
+      </div>
+    </div>
+  );
+};
+
+export default MealSummary;
