6d5d590a4673870167d75b8bb935edbe9e5064b5
rtjae1 <ryan.t.jae@gmail.com>
2019-01-23 11:11:59 -0500

    navbar

---
+++ mealplan/src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Meal</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Sign Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating pink lighten-1">
+          RJ
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
