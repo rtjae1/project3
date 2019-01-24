519c281178b63f2a6b1618480c335dbce1cb7953
rtjae1 <ryan.t.jae@gmail.com>
2019-01-24 09:58:13 -0500

    user sign up

--- mealplan/src/store/reducers/authReducer.js
+++ mealplan/src/store/reducers/authReducer.js
@@ -19,6 +19,18 @@ const authReducer = (state = initState, action) => {
     case "SIGNOUT_SUCCESS":
       console.log("signout success");
       return state;
+    case "SIGNUP_SUCCESS":
+      console.log("sign up success");
+      return {
+        ...state,
+        authError: null
+      };
+    case "SIGNUP_ERROR":
+      console.log("sign up error");
+      return {
+        ...state,
+        authError: action.err.message
+      };
     default:
       return state;
   }
