914d686d7da57c93faf4a1b7a3e683d7125805b7
rtjae1 <ryan.t.jae@gmail.com>
2019-01-23 21:36:00 -0500

    firebase auth

--- mealplan/src/store/reducers/rootReducer.js
+++ mealplan/src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from "./authReducer";
 import mealReducer from "./mealReducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   meal: mealReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
