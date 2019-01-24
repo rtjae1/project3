dc0af3cdc491fa50da90c79addcbc088153a3bb6
rtjae1 <ryan.t.jae@gmail.com>
2019-01-23 21:01:31 -0500

    project details

--- mealplan/src/components/meals/MealDetails.js
+++ mealplan/src/components/meals/MealDetails.js
@@ -1,26 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const MealDetails = props => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section-meal-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Meal Title - {id}</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            repellendus nemo illo eveniet accusantium aspernatur nesciunt
-            molestiae earum harum reiciendis, iste aliquam, debitis fuga
-            molestias non delectus eius cupiditate blanditiis.
-          </p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by wRyan</div>
-          <div>January 23</div>
+  const { meal } = props;
+  if (meal) {
+    return (
+      <div className="container section meal-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{meal.dayOfWeek}</span>
+            <p>{meal.meal}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by {meal.authorFirstName} {meal.authorLastName}
+            </div>
+            <div>January 23</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading mealz..</p>
+      </div>
+    );
+  }
 };
 
-export default MealDetails;
+const mapStateToProps = (state, ownProps) => {
+  // console.log(state);
+  const id = ownProps.match.params.id;
+  const meals = state.firestore.data.meals;
+  const meal = meals ? meals[id] : null;
+  return {
+    meal: meal
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "meals" }])
+)(MealDetails);
--- mealplan/src/components/meals/MealList.js
+++ mealplan/src/components/meals/MealList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import MealSummary from "./MealSummary";
+import { Link } from "react-router-dom";
 
 const MealList = ({ meals }) => {
   return (
     <div className="meal-list section">
       {meals &&
         meals.map(meal => {
-          return <MealSummary meal={meal} key={meal.id} />;
+          return (
+            <Link to={"/project/" + meal.id} key={meal.id}>
+              <MealSummary meal={meal} />
+            </Link>
+          );
         })}
     </div>
   );
