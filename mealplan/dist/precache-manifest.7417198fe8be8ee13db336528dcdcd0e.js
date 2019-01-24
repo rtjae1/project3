08d0c09a373158426bd76427046a1f25f905c521
rtjae1 <ryan.t.jae@gmail.com>
2019-01-24 13:52:46 -0500

    deployment folder structure

---
+++ mealplan/dist/precache-manifest.7417198fe8be8ee13db336528dcdcd0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2d72474dde1d78a0370",
+    "url": "/static/js/main.c2d72474.chunk.js"
+  },
+  {
+    "revision": "0c59a647577edb0beed5",
+    "url": "/static/js/1.0c59a647.chunk.js"
+  },
+  {
+    "revision": "c2d72474dde1d78a0370",
+    "url": "/static/css/main.b25abeab.chunk.css"
+  },
+  {
+    "revision": "84a1407ee424980e835fbce3c3483244",
+    "url": "/index.html"
+  }
+];
