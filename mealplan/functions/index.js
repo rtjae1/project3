65300332472bffb397181a5319803129db17383e
rtjae1 <ryan.t.jae@gmail.com>
2019-01-24 12:59:39 -0500

    more triggers/auth signup

--- mealplan/functions/index.js
+++ mealplan/functions/index.js
@@ -26,3 +26,21 @@ exports.mealCreated = functions.firestore
 
     return createNotification(notification);
   });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Joined the table!",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
+    });
+});
