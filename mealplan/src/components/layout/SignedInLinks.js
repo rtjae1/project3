edf4e6d09588628eefdca1c311d97f214e653354
rtjae1 <ryan.t.jae@gmail.com>
2019-01-24 10:18:10 -0500

    user profile data

--- mealplan/src/components/layout/SignedInLinks.js
+++ mealplan/src/components/layout/SignedInLinks.js
@@ -14,7 +14,7 @@ const SignedInLinks = props => {
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
-          RJ
+          {props.profile.initials}
         </NavLink>
       </li>
     </ul>
